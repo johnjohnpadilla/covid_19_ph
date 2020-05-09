a94dfad9639aeaac15504891076fc0d03f129b72
JohnPadilla <john-frederick.padilla@dxc.com>
2020-05-09 17:42:58 +0800

    remove request module

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import requests
-import request as req
 import streamlit as st
 import plotly.express as px
 from geopy.geocoders import Nominatim
